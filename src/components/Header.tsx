33f08595d9f7559adfefbdc7380ca8a46a105a4e
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-05-12 18:23:42 -0300

    add headlessui menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,19 @@
-import Link from 'next/link';
-import { Menu } from '@headlessui/react'
+import Link from "next/link";
+import { OptionsButton } from "./OptionsButton";
 
-export const Header = () => {
+
+export function Header() {
   return (
-    <div className="bg-zinc-900 text-slate-200 flex flex-wrap justify-around items-center p-7">
+    <div className="flex items-center justify-around p-7">
       <Link href="/">
-        <span className="text-3xl cursor-pointer"><span className="text-blue-700">L</span>earning.<span className="text-red-700">M</span>apper</span>
+        <span className="text-3xl">Learning.Mapper</span>
       </Link>
-      <nav>
-        <ul className="flex text-xl">
-          <li className="ml-10 cursor-auto hover:text-blue-500 transition"><Link className="block p-3" href="/casa">Testes</Link></li>
-          <li className="ml-10 cursor-auto hover:text-blue-500 transition"><Link className="block p-3 " href="/carro">Relatórios</Link></li>
-          <li className="ml-10 cursor-auto hover:text-blue-500 transition"><Link className="block p-3 " href="/moto">Opções</Link></li>
-        </ul>
-      </nav>
+      <div className="flex items-center gap-4 hover:text-slate-300">
+        <OptionsButton />
+        |
+        <Link href="/">Sobre</Link>
       </div>
-    )
+    </div>
+
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,8 @@ const Home: NextPage = () => {
   return (
   <>
     <Header />
-      <div className=" w-[70vw] h-[70vh] my-[9vh] mx-auto border-8 border-zinc-500 rounded-lg">
-        
+      <div className=" w-[70vw] h-[70vh] my-[9vh] mx-auto border-8 border-zinc-300 rounded-lg">
+
     </div>
   </>
    )
